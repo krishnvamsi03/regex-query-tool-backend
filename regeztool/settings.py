ec2e00599c1b401480d924915afa943b887a6869
Krishnavamsi <vamsik532@gmail.com>
2021-05-29 21:05:47 +0530

    Deploying change

--- regeztool/settings.py
+++ regeztool/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '793xxistm-n=&^@dta22hy@8+k4=c^(3)uxv6_^5oy6mh#nctq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
